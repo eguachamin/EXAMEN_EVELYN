a8e33fb330c40d5da51f84f7fd44935359f2444b
POO <taty-1997-02@outlook.com>
2024-06-14 13:07:48 -0500

    commit Evelyn

--- EL_BUHO/Main.java
+++ EL_BUHO/Main.java
@@ -6,19 +6,19 @@ public class Main {
         Normal_VIP persona1 =new Normal_VIP(1751253400,"Evelyn",3393346,"eve@gmail.com",
                 "Normal","Quito-Puyo","Ida",15.62,1,"Sentada");
         persona1.mostrarDatos();
-
+        System.out.println("----------------------");
         Normal_VIP persona2 =new Normal_VIP(1751253400,"Andrea",3393346,"eve@gmail.com",
                 "Normal","Quito-Puyo","Ida",15.62,1,"Sentada");
         persona2.mostrarDatos();
-
+        System.out.println("----------------------");
         Normal_VIP persona3 =new Normal_VIP(1751253400,"Sebastian",3393346,"eve@gmail.com",
                 "Normal","Quito-Puyo","Ida",15.62,1,"Sentada");
         persona3.mostrarDatos();
-
+        System.out.println("----------------------");
         Normal_VIP persona4 =new Normal_VIP(1751253400,"Ariel",3393346,"eve@gmail.com",
                 "Normal","Quito-Puyo","Ida",15.62,1,"Sentada");
         persona4.mostrarDatos();
-
+        System.out.println("----------------------");
         Normal_VIP persona5 =new Normal_VIP(1751253400,"Andrea",3393346,"eve@gmail.com",
                 "Normal","Quito-Puyo","Ida",15.62,1,"Sentada");
         persona5.mostrarDatos();
